34e2810bbf78749f913b130bdec50da2122441d1
zoranbogoevski <zoran@orangemelos.com>
2023-10-20 09:17:04 +0200

    first commit

---
+++ src/Http/Controllers/PaymentController.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace Kalimeromk\HalkbankPayment\Http\Controllers;
+
+use App\Http\Controllers\Controller;
+
+class PaymentController extends Controller{
+    public function index()
+    {
+        //
+    }
+}
